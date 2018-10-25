fdf27351c26fea593c7138e258e11d15a257f866
Manish <manish@manishs-imac.home>
2018-10-25 21:18:44 +0100

    fix checklist warnings2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,11 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-  
-/**
-* @param someone name of the person 
-* @return getting string
-*/
+
+ /**
+  * @param someone name of the person example
+  * @return getting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
